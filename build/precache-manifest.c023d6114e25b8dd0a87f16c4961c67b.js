3f9979af6a2a1e1c565273afb7eaad9f945b68fb
tlylt <liu_yongliang@hotmail.com>
2019-10-27 10:37:13 +0800

    Uploaded production build

---
+++ build/precache-manifest.c023d6114e25b8dd0a87f16c4961c67b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea87bb5b32962198519ae67a2a5ba437",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80a504162213c8bb752b",
+    "url": "/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "80a504162213c8bb752b",
+    "url": "/static/js/2.fe80d79f.chunk.js"
+  },
+  {
+    "revision": "2a1234ca90981fe3844f",
+    "url": "/static/js/main.8bd19f9f.chunk.js"
+  },
+  {
+    "revision": "f1b768174fa530eb5144",
+    "url": "/static/js/runtime-main.39dd3371.js"
+  }
+]);
